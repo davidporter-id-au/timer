5cfbb1b9fabe924a606618603cea2cc69f3e375e
DesertLynx <contact@davidporter.id.au>
2014-06-25 19:03:03 +1000

    Added half-working basecamp integration

---
+++ basecamp.js
@@ -0,0 +1,79 @@
+
+var 	MongoClient = require('mongodb').MongoClient, 
+	format = require('util').format,  
+	moment = require('moment'), 
+	prompt = require('prompt'),
+	config = require('./config'), 
+	fs = require('fs'), 
+	request = require('request-sync');
+
+if(process.argv.length >= 2)
+	var company  = process.argv[2];
+
+if(process.argv.length >= 3)
+	if(process.argv[3] == 'save')
+		var save = process.argv[3];
+
+//Prompt schema
+var schema = {
+	properties: {
+		password: {
+			message: 'password for ' + config.basecampUsername,
+			hidden: true
+		}
+	}
+};
+
+// Start the prompt
+prompt.start();
+
+// Get two properties from the user: email, password
+prompt.get(schema, function (err, input) { 
+
+	if(err) throw err;
+
+	MongoClient.connect(config.mongo, function(err, db) {
+		if(err) throw err;
+
+		db.collection('entries').find({company: company, fetched: null, uploaded: null})
+			.toArray(function(e, res){
+			if(!e) {
+
+				for(var i = 0; i < res.length; i++) {
+
+					//Create an xml object to send:
+					var data = "<time-entry><person-id>" + config.basecampUserid + "</person-id><date>" + moment(res[i].starttime).toISOString() + "</date><hours>" + ((Math.round((res[i].endtime - res[i].starttime) / 1000 / 60) * 10 ) / 10) / 60; + "</hours><description>" + res[i].commit + "</description></time-entry>";
+					console.log(data);
+
+					//Send it. Note this is a blocking request
+					var result = request({url: config.basecamp, body: data, method: 'POST', headers: { "Content-type": 'application/xml'}, auth:{username: config.basecampUsername, pass: input.password}});
+					console.log('Entry id:', res[i]._id);
+					console.log(result.body);
+				}
+				
+				//if requested to save, it'll mark the entries as 'saved' to prevent them coming up again
+				if(save) {
+					db.collection('entries').update(
+						{company: company, fetched: null}, 
+						{$set: {uploaded: true}}, 
+						{multi: true},
+						function(e){
+							console.error(e);
+							db.close();
+						});
+					}
+				else {
+					db.close();
+				}
+			}
+
+		});
+
+	});
+
+
+});
+
+
+
+
---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	rate: 30,
+	name: 'David Porter',
+	abn: 'asdfasdf', 
+	mongo: 'mongodb://127.0.0.1:27017/timer', 
+	//basecamp: 'https://mfdc.basecamphq.com/projects/12146784-client-voltron-hylc/time_entries.xml',
+	basecamp: 'https://mfdc.basecamphq.com/projects/12207945-mfdc-voltron-mfdc/time_entries.xml',
+	basecampUserid: 10085201,
+	basecampUsername: 'david@mfdc.biz',
+};
